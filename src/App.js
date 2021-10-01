06963f9476db8631285949120c5f9a7da65e6bf6
JamesSoFer <solisjr1919@gmail.com>
2021-09-30 19:46:15 -0600

    --Agrega Navbar/ Mobile-Desktop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home'; // Do the same thing to import the other pages
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
