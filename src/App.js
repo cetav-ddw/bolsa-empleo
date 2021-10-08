ef7c45f6e1562ecbc387aac44b25791b45d9eed0
JamesSoFer <solisjr1919@gmail.com>
2021-10-08 00:16:33 -0600

    feat: Navbar responsive/ Styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home'; // Do the same thing to import the other pages
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-        </Switch>
-      </Router>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Container, LogoContainer, Wrapper, Menu, MenuItem, MenuItemLink, MobileIcon } from "./Narbar.elements";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
+const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  return (
+    <Container>
+      <Wrapper>
+        <IconContext.Provider value={{ style: { fontSize: "2em" } }}>
+          <LogoContainer>
+            <img src="./images/triada-logo.png"/>
+            <p>Bolsa de empleo</p>  
+          </LogoContainer>
+
+          <MobileIcon onClick={() => setShowMobileMenu(!showMobileMenu)}>
+            {showMobileMenu ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+
+          <Menu open={showMobileMenu}>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <img src="./images/home.png"/>
+                <div>
+                  Inicio
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <img src="./images/ic_baseline-post-add.png"/>
+                <div>
+                  Publicar Oferta
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <img src="./images/card_travel.png"/>
+                <div>
+                  Ofertas Laborales
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <img src="./images/home.png"/>
+                <div>
+                  Cetav Principal
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+          </Menu>
+        </IconContext.Provider>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
