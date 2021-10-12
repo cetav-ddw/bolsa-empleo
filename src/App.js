04699eae629dc39f380daa0914ed7f6c68db9efc
Kendoll Castro <kendollcastro@gmail.com>
2021-10-11 21:37:06 -0600

    Agrego mi primera parte de la vista del jobs views

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Jobsview from './components/Jobsview';
 
 function App() {
   return (
     <div className="App">
-      
+      <Jobsview />
     </div>
   );
 }
