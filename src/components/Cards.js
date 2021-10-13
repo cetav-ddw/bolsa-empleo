9ba5568d0c980d52b9e572e2ef20675590837dc9
Jean Brown <jeantoruno00@gmail.com>
2021-10-12 22:30:57 -0700

    Update MediaQuerrys

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -27,91 +27,205 @@ function Cards() {
                     </Wrapper>
                 ))}
         </div>
-        /* <Wrapper>
-            <Jobs src="img/jobs.png" />
-            <Content>
-                <Title>{post.career}</Title>
-                <ContainerText><ContentType>{post.type_job} {post.job_level} {post.description}</ContentType></ContainerText>
-            </Content>
-        </Wrapper> */
     )
 }
 
 const Wrapper = styled.div`
-    border-radius: 10px 0px;
-    border: 2px solid #5E5E5E;
-    display: flex;
-    height: 177px;
-    justify-content: space-between;
-    margin-top: 200px;
-    margin: 0 auto;
-    width: 343px;
+    @media(min-width:1080px){
+        border-radius: 10px 0px;
+        border: 2px solid #5E5E5E;
+        justify-content: space-between;
+        margin-top: 200px;
+        margin: 22px auto;
+        width: 333px;
+    }
+    @media(max-width:834px){
+        border-radius: 10px 0px;
+        border: 2px solid #5E5E5E;
+        display: flex;
+        height: 177px;
+        justify-content: space-between;
+        margin-top: 200px;
+        margin: 22px auto;
+        width: 770px;
+    }
+    @media(max-width:375px){
+        border-radius: 10px 0px;
+        border: 2px solid #5E5E5E;
+        display: flex;
+        height: 177px;
+        justify-content: space-between;
+        margin-top: 200px;
+        margin: 22px auto;
+        width: 343px;
+    }
 `
 const Content = styled.div`
-    align-items: start; 
-    display: flex;
-    flex-direction: column;
-    height: 139px;
-    margin-right: 16px;
-    width: 187px;
+    @media(min-width:1080px){
+        width: 265px;
+    }
+    @media(max-width:834px){
+        align-items: start; 
+        display: flex;
+        flex-direction: column;
+        height: 139px;
+        margin-right: 24px;
+        width: 616px;
+    }
+    @media(max-width:375px){
+        align-items: start; 
+        display: flex;
+        flex-direction: column;
+        height: 139px;
+        margin-right: 16px;
+        width: 187px;
+    }
 `
 
 const ContainerText = styled.div`
-    align-items: start; 
-    text-align: left;
-    width: 185px;
+    @media(min-width:1080px){
+        text-align: left;
+        width: 318px;
+    }
+    @media(max-width:834px){
+        align-items: start; 
+        text-align: left;
+        width: 456px;
+    }
+    @media(max-width:375px){
+        align-items: start; 
+        text-align: left;
+        width: 185px;
+    }
 `
 
 const Title = styled.div`
-    color: #5755CC;
-    font-family: Open Sans;
-    font-size: 11px;
-    font-style: normal;
-    font-weight: bold;
-    line-height: 15px;
-    margin-top: 8px;
-    text-transform: uppercase;
+    @media(min-width:1080px){
+        color: #5755CC;
+        font-family: Open Sans;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 15px;
+        margin-top: 24px;
+        text-transform: uppercase;
+    }
+    @media(max-width:834px){
+        color: #5755CC;
+        font-family: Open Sans;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 15px;
+        margin-top: 24px;
+        text-transform: uppercase;
+    }
+    @media(max-width:375px){
+        color: #5755CC;
+        font-family: Open Sans;
+        font-size: 11px;
+        font-style: normal;
+        font-weight: bold;
+        line-height: 15px;
+        margin-top: 8px;
+        text-transform: uppercase;
+    }
 `
 
 const ContentType = styled.p`
-    color: #292929;
-    font-family: Open Sans;
-    font-size: 18px;
-    font-style: normal;
-    font-weight: normal;
-    height: 75px;
-    left: 0px;
-    letter-spacing: 0.005em;
-    line-height: 25px;
-    top: 24px;
-    width: 185px;
+    @media(min-width:1080px){
+        color: #5E5E5E;
+        font-family: Open Sans;
+        font-size: 26px;
+        font-style: normal;
+        font-weight: normal;
+        height: 75px;
+        left: 0px;
+        letter-spacing: 0.005em;
+        line-height: 25px;
+        margin-left: 14px;
+        top: 24px;
+        width: 340px;
+    }
+    @media(max-width:834px){
+        color: #5E5E5E;
+        font-family: Open Sans;
+        font-size: 26px;
+        font-style: normal;
+        font-weight: normal;
+        height: 75px;
+        left: 0px;
+        letter-spacing: 0.005em;
+        line-height: 25px;
+        top: 24px;
+        width: 340px;
+    }
+    @media(max-width:375px){
+        color: #292929;
+        font-family: Open Sans;
+        font-size: 18px;
+        font-style: normal;
+        font-weight: normal;
+        height: 75px;
+        left: 0px;
+        letter-spacing: 0.005em;
+        line-height: 25px;
+        top: 24px;
+        width: 185px;
+    }
 `
 const Togletags = styled.div`
-    align-items: center;
-    background: #E1ECF4;
-    border-radius: 5px;
-    color: #0C4066;
-    display: flex;
-    font-size: 10px;
-    height: 24px;
-    justify-content: center;
-    margin-left: 8px;
-    text-transform: uppercase;
-    width: 60px;
+    @media(max-width:834px){
+    }
+    @media(max-width:375px){
+        align-items: center;
+        background: #E1ECF4;
+        border-radius: 5px;
+        color: #0C4066;
+        display: flex;
+        font-size: 10px;
+        height: 24px;
+        justify-content: center;
+        margin-left: 8px;
+        text-transform: uppercase;
+        width: 60px;
+    }
 `
 
 const WrapperTags = styled.div`
-    display: flex;
-    flex-direction: row;
+    @media(max-width:834px){
+        
+    }
+    @media(max-width:375px){
+        display: flex;
+        flex-direction: row;
+    }
 `
 
 const Jobs = styled.img`
-    border-radius: 5px;
-    float: left;
-    height: 100px;
-    margin-left: 16px;
-    margin-top: 38px;
-    width: 100px;
+    @media(min-width:1080px){
+        border-radius: 5px;
+        float: left;
+        height: 187px;
+        margin: 16px 16px;
+        width: 300px;
+    }
+    @media(max-width:834px){
+        border-radius: 5px;
+        float: left;
+        height: 100px;
+        margin-left: 16px;
+        margin-top: 38px;
+        width: 100px;
+    }
+    @media(max-width:375px){
+        border-radius: 5px;
+        float: left;
+        height: 100px;
+        margin-left: 16px;
+        margin-top: 38px;
+        width: 100px;
+    }
 `
 
 export default Cards;
